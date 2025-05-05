0ac7e59030ab13b938e81f4e7b0cd0a767d67299
artscouser <artemmkr98@mail.ru>
2025-05-05 19:51:42 +0400

    add FirstTask

--- TypeOperations/Program.cs
+++ TypeOperations/Program.cs
@@ -1,9 +1,37 @@
 ﻿namespace TypeOperations;
+using System;
 
-class Program
+static class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.InputEncoding = System.Text.Encoding.UTF8;
+        //Divide(5,0);
+        int a = RoundAndCheck(double.MaxValue);
+        Console.WriteLine(a);
+        Console.WriteLine(double.MaxValue);
+    }
+
+    static double Divide(int a, double b)
+    {
+        if (b == 0)
+        {
+            throw new ArgumentException("Divide by zero");
+        }
+
+        Console.WriteLine(a / b);
+
+        return a / b;
+    }
+
+    static int RoundAndCheck(double value)
+    {
+        return checked((int)value);
     }
 }
+
+
+
+
+
