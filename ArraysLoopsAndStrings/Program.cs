5f80ec284728f75c5ac4a94bd9630043dd900e11
artscouser <artemmkr98@mail.ru>
2025-05-12 21:02:17 +0400

    add factorial task

--- ArraysLoopsAndStrings/Program.cs
+++ ArraysLoopsAndStrings/Program.cs
@@ -3,7 +3,17 @@
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
+    {    	
+		Console.WriteLine(GetFactorial(100000));
     }
+	
+	static int GetFactorial(int n)
+	{
+		if(n == 1)
+		{
+			return 1;	
+		}
+		
+		return n * GetFactorial(n - 1);
+	}
 }
